7a72e394e68db9aa64d776e9f99188a38d2becb7
AkbarHuseynli <akbar.hnl27@gmail.com>
2023-08-29 18:15:33 +0400

    user authenticate

--- src/main/java/org/example/app/service/UserService.java
+++ src/main/java/org/example/app/service/UserService.java
@@ -1,4 +1,7 @@
 package org.example.app.service;
 
+import org.example.app.entity.User;
+
 public interface UserService {
+    User authenticate(String username, String password);
 }
--- src/main/java/org/example/app/service/UserServiceImpl.java
+++ src/main/java/org/example/app/service/UserServiceImpl.java
@@ -2,7 +2,19 @@ package org.example.app.service;
 
 import org.example.app.dao.UserDao;
 import org.example.app.dao.UserDaoImpl;
+import org.example.app.entity.User;
 
 public class UserServiceImpl implements UserService {
     private final UserDao userDao = new UserDaoImpl();
+
+    @Override
+    public User authenticate(String username, String password) {
+
+        return userDao.getAll()
+                .stream()
+                .findAny()
+                .filter(user ->
+                        user.getUserName().equalsIgnoreCase(username)&&
+                                user.getPassword().equalsIgnoreCase(password)).get();
+    }
 }
