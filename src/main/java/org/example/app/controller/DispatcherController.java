756d7757f4f968c1999e14102a8dbbc4e5661a88
XeyalQaniyev <qaniyevx@gmail.com>
2023-08-30 16:03:01 +0400

    menu and input

--- src/main/java/org/example/app/controller/DispatcherController.java
+++ src/main/java/org/example/app/controller/DispatcherController.java
@@ -3,13 +3,15 @@ package org.example.app.controller;
 import org.example.app.constant.LoginRegister;
 import org.example.app.constant.Menu;
 import org.example.app.constant.SearchAndBook;
+import org.example.app.entity.Reservation;
 import org.example.app.entity.User;
 
 import java.util.Arrays;
 
-import static org.example.app.util.MenuUtil.getIndex;
+import static org.example.app.util.MenuUtil.*;
 
-    public class DispatcherController {
+public class DispatcherController {
+
 
         private final FlightController flightController = new FlightControllerImpl();
         private final UserController userController = new UserControllerImpl();
@@ -20,12 +22,22 @@ import static org.example.app.util.MenuUtil.getIndex;
             int index = getIndex();
             switch (index){
                 case 1->{
-
+                    System.out.print("Enter username: ");
+                    String userName = getInput();
+                    System.out.print("Enter password: ");
+                    String password = getInput();
+                    User user = userController.authenticate(userName,password);
+                    if (user != null){
+                        selectMenu(user);
+                    }else {
+                        loginRegister();
+                    }
                 }
             }
         }
 
-        public  void selectMenu(){
+        public  void selectMenu(User user){
+            Reservation reservation;
             boolean flag=true;
             while (flag){
                 showMenu();
@@ -33,23 +45,23 @@ import static org.example.app.util.MenuUtil.getIndex;
                 switch (index){
                     case 1->{
                         flightController.showAll();
-                        selectMenu();
+                        selectMenu(user);
                     }
                     case 2->{
                         System.out.println("Enter flight id:");
                         int flightId = getIndex();
                         System.out.println(flightController.getFlightById(flightId));
-                        selectMenu();
+                        selectMenu(user);
                     }
                     case 3->{
                         showSearchAndBookMenu();
-                        int menuinp = getIndex();
-                        switch (menuinp){
+                        int menuInp = getIndex();
+                        switch (menuInp){
                             case 1->{
                                 flightController.searchFlight();
                             }
                             case 2->{
-                                reservationController.bookFlight(null);
+
                             }
                         }
                     }
@@ -57,7 +69,7 @@ import static org.example.app.util.MenuUtil.getIndex;
                         reservationController.cancelFlight(null);
                     }
                     case 5->{
-                        userController.showMyFlights();
+                        userController.showMyFlights((int) user.getId());
                     }
                     case 6->flag=false;
                     default -> System.out.println("ENTER VALID COMMAND");
