26d33a35a3673cf4d3c6e2f02b70bb1bc7d8ad24
XeyalQaniyev <qaniyevx@gmail.com>
2023-08-30 12:29:37 +0400

    menu and input

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -1,5 +1,6 @@
 package org.example.app;
 
+import org.example.app.controller.DispatcherController;
 import org.example.app.dao.*;
 import org.example.app.entity.Flight;
 import org.example.app.entity.Reservation;
@@ -10,8 +11,46 @@ import org.example.app.util.Util;
 import java.time.LocalDate;
 
 public class Main {
+    private static final DispatcherController dispatcherController = new DispatcherController();
     public static void main(String[] args) {
+run();
+//        MenuUtil.logging("name", "sdsa");
+//        System.out.println(MenuUtil.loggedUserId);
+        //passed loggin
+
+        //Online board
+//        FlightDao flightDao = new FlightDaoImpl();
+//        flightDao.showAll();
+
+        //SHOW THE FLIGHT INFO
+//        System.out.println(flightDao.getFlightById(3));
+
+//        //search and book
+//        flightDao.searchFlight("British", LocalDate.ofEpochDay(2025-8-26),5);
+//
+//        ReservationDao reservationDao = new ReservationDaoImpl();
+//        UserDao user = new UserDaoImpl();
+//        User client = user.getUserById(8);
+//        Flight flight = flightDao.getFlightById(5);
+////        System.out.println(flight.getId());
+//        Reservation res = new Reservation(client,flight,10);
+//        reservationDao.bookFlight(res);
+
+//
+//        //cancel
+//        reservationDao.cancelFlight(res);
+
+//        System.out.println(Util.uploadFlight(FilePath.FLIGHT_LIST_DOC.getValue());
 
 
     }
+
+    private static void run() {
+        System.out.println("WELCOME TO APPLICATION");
+
+        dispatcherController.selectMenu();
+
+        System.out.println("GOOD BYE");
+    }
+
 }
--- src/main/java/org/example/app/controller/DispatcherController.java
+++ src/main/java/org/example/app/controller/DispatcherController.java
@@ -1,4 +1,79 @@
 package org.example.app.controller;
 
-public class DispatcherController {
-}
+import org.example.app.constant.LoginRegister;
+import org.example.app.constant.Menu;
+import org.example.app.constant.SearchAndBook;
+import org.example.app.entity.User;
+
+import java.util.Arrays;
+
+import static org.example.app.util.MenuUtil.getIndex;
+
+    public class DispatcherController {
+
+        private final FlightController flightController = new FlightControllerImpl();
+        private final UserController userController = new UserControllerImpl();
+        private final ReservationController reservationController = new ReservationControllerImpl();
+
+        public void loginRegister(){
+            showLoginAndRegisterMenu();
+            int index = getIndex();
+            switch (index){
+                case 1->{
+
+                }
+            }
+        }
+
+        public  void selectMenu(){
+            boolean flag=true;
+            while (flag){
+                showMenu();
+                int index = getIndex();
+                switch (index){
+                    case 1->{
+                        flightController.showAll();
+                        selectMenu();
+                    }
+                    case 2->{
+                        System.out.println("Enter flight id:");
+                        int flightId = getIndex();
+                        System.out.println(flightController.getFlightById(flightId));
+                        selectMenu();
+                    }
+                    case 3->{
+                        showSearchAndBookMenu();
+                        int menuinp = getIndex();
+                        switch (menuinp){
+                            case 1->{
+                                flightController.searchFlight();
+                            }
+                            case 2->{
+                                reservationController.bookFlight(null);
+                            }
+                        }
+                    }
+                    case 4->{
+                        reservationController.cancelFlight(null);
+                    }
+                    case 5->{
+                        userController.showMyFlights();
+                    }
+                    case 6->flag=false;
+                    default -> System.out.println("ENTER VALID COMMAND");
+                }
+            }
+        }
+        private void showMenu() {
+            Arrays.stream(Menu.values())
+                    .forEach(it -> System.out.printf("%d-%s\n", it.getIndex(), it.getDescription()));
+        }
+        private void showSearchAndBookMenu() {
+            Arrays.stream(SearchAndBook.values())
+                    .forEach(it -> System.out.printf("%d-%s\n", it.getIndex(), it.getDescription()));
+        }
+        private void showLoginAndRegisterMenu(){
+            Arrays.stream(LoginRegister.values())
+                    .forEach(it -> System.out.printf("%d-%s\n", it.getIndex(), it.getDescription()));
+        }
+    }
