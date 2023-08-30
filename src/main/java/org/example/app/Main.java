765a0a7d308722fd954e110b1b4252f1919df8a1
UmbrellaClon <qaniyevx@gmail.com>
2023-08-30 19:07:39 +0400

    change cancelFlight

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -1,17 +1,7 @@
 package org.example.app;
 
-import org.example.app.constant.FilePath;
 import org.example.app.controller.DispatcherController;
-import org.example.app.controller.FlightController;
-import org.example.app.controller.FlightControllerImpl;
-import org.example.app.dao.*;
-import org.example.app.entity.Flight;
-import org.example.app.entity.Reservation;
-import org.example.app.entity.User;
-import org.example.app.util.MenuUtil;
 import org.example.app.util.Util;
-
-import java.time.LocalDate;
 
 public class Main {
     private static final DispatcherController dispatcherController = new DispatcherController();
